1fab6b63a3fed3768c53dd8cf513a94cc06f24bd
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-02-14 15:07:00 +0200

    feat: add Dialogs component

--- src/components/Pages/Dialogs/Dialogs.jsx
+++ src/components/Pages/Dialogs/Dialogs.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import cls from './Dialogs.module.sass'
+
+const DialogItem = ({ name, id }) => {
+  return (
+    <div className={cls.dialog}>
+      <NavLink to={`/dialogs/${id}`} activeClassName={cls.active}>
+        {name}
+      </NavLink>
+    </div>
+  )
+}
+
+const Message = ({ message }) => {
+  return(
+    <div className={cls.message}>{message}</div>
+  )
+}
 
 const Dialogs = () => {
   return (
     <div className={cls.dialogs}>
-      Dialogs
+      <div className={cls.dialogsItems}>
+        <DialogItem name='Name' id='1' />
+        <DialogItem name='Namee' id='2' />
+        <DialogItem name='Nameee' id='3' />
+        <DialogItem name='Nameeee' id='4' />
+      </div>
+      <div className={cls.messages}>
+        <Message message = 'Hi!' />
+        <Message message = 'Whats Upppp' />
+        <Message message = 'Yoooooooooou' />
+      </div>
     </div>
   )
 }
