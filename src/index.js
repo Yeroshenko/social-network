a57effcddefc13754166334e2fbdaadbd3b1eb7c
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-02-19 23:52:11 +0200

    ref: OOP approach for store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import state from './redux/state'
-import { subscribe, addPost, updateNewPostText } from './redux/state'
-
+import store from './redux/state'
 import App from './App'
 
 import './index.sass'
@@ -12,14 +10,14 @@ const rerenderEntireTree = (state) => {
   const app = (
     <App 
       state={state} 
-      addPost={addPost} 
-      updateNewPostText={updateNewPostText} 
+      addPost={store.addPost.bind(store)} 
+      updateNewPostText={store.updateNewPostText.bind(store)} 
     /> 
   )
 
   ReactDOM.render(app, document.getElementById('root'))
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,127 +1,132 @@
-let rerenderEntireTree = () => {
-  console.log('state changed')
-}
-
-const state = {
-  profilePage: {
-    posts: [{
+const store = {
+  _state: {
+    profilePage: {
+      posts: [{
+          id: 1,
+          message: 'Hi, hoe are you?',
+          likesCount: 12
+        },
+        {
+          id: 2,
+          message: "It's my first post",
+          likesCount: 10
+        },
+        {
+          id: 3,
+          message: 'Hello world',
+          likesCount: 228
+        }
+      ],
+      profile: {
         id: 1,
-        message: 'Hi, hoe are you?',
-        likesCount: 12
+        name: 'Valeriy Yeroshenko',
+        site: 'yeroshenko.github.io',
       },
-      {
-        id: 2,
-        message: "It's my first post",
-        likesCount: 10
-      },
-      {
-        id: 3,
-        message: 'Hello world',
-        likesCount: 228
-      }
-    ],
-    profile: {
-      id: 1,
-      name: 'Valeriy Yeroshenko',
-      site: 'yeroshenko.github.io',
+      newPostText: ''
     },
-    newPostText: ''
+    dialogsPage: {
+      messages: [{
+          id: 1,
+          message: 'Hi!'
+        },
+        {
+          id: 2,
+          message: 'Whats Upppp'
+        },
+        {
+          id: 3,
+          message: 'Yoooooooooo'
+        }
+      ],
+      dialogs: [{
+          id: 1,
+          name: 'Name'
+        },
+        {
+          id: 2,
+          name: 'Namee'
+        },
+        {
+          id: 3,
+          name: 'Nameee'
+        },
+        {
+          id: 4,
+          name: 'Nameeee'
+        },
+        {
+          id: 5,
+          name: 'Nameeeee'
+        },
+        {
+          id: 6,
+          name: 'Nameeeeee'
+        }
+      ]
+    },
+    asidePage: {
+      friends: [
+        {
+          id: 2,
+          name: 'Name1 Surname1',
+          site: 'website1.io',
+        },
+  
+        {
+          id: 3,
+          name: 'Name2 Surname2',
+          site: 'website2.io',
+        },
+  
+        {
+          id: 4,
+          name: 'Name3 Surname3',
+          site: 'website3.io',
+        },
+  
+        {
+          id: 5,
+          name: 'Name4 Surname4',
+          site: 'website4.io',
+        },
+  
+        {
+          id: 6,
+          name: 'Name5 Surname5',
+          site: 'website5.io',
+        },
+      ]
+    }
   },
-  dialogsPage: {
-    messages: [{
-        id: 1,
-        message: 'Hi!'
-      },
-      {
-        id: 2,
-        message: 'Whats Upppp'
-      },
-      {
-        id: 3,
-        message: 'Yoooooooooo'
-      }
-    ],
-    dialogs: [{
-        id: 1,
-        name: 'Name'
-      },
-      {
-        id: 2,
-        name: 'Namee'
-      },
-      {
-        id: 3,
-        name: 'Nameee'
-      },
-      {
-        id: 4,
-        name: 'Nameeee'
-      },
-      {
-        id: 5,
-        name: 'Nameeeee'
-      },
-      {
-        id: 6,
-        name: 'Nameeeeee'
-      }
-    ]
+
+  getState() {
+    return this._state
   },
-  asidePage: {
-    friends: [
-      {
-        id: 2,
-        name: 'Name1 Surname1',
-        site: 'website1.io',
-      },
 
-      {
-        id: 3,
-        name: 'Name2 Surname2',
-        site: 'website2.io',
-      },
+  _callSubscriber() {
+    console.log('state changed')
+  },
 
-      {
-        id: 4,
-        name: 'Name3 Surname3',
-        site: 'website3.io',
-      },
+  addPost() {
+    const newPost = {
+      id: 4,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    }
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = ''
+    this._callSubscriber(this._state)
+  },
 
-      {
-        id: 5,
-        name: 'Name4 Surname4',
-        site: 'website4.io',
-      },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state)
+  },
 
-      {
-        id: 6,
-        name: 'Name5 Surname5',
-        site: 'website5.io',
-      },
-    ]
-  }
+  subscribe(observer) {
+    this._callSubscriber = observer 
+  } 
 }
 
-export const addPost = () => {
-  const newPost = {
-    id: 4,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  }
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText = ''
-  rerenderEntireTree(state)
-
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree(state)
-
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer 
-} 
-
-export default state
+export default store
+window.store = store
