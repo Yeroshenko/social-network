35a0735906852566be44b4744e3a0beb0755c8a9
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-02-24 18:31:24 +0200

    ref: connected axios, received a list of users from the server

--- src/components/Pages/Users/Users.jsx
+++ src/components/Pages/Users/Users.jsx
@@ -1,66 +1,17 @@
 import React from 'react'
+import * as axios from 'axios'
+
+import userPhoto from '../../../assets/img/user.png'
 
 import cls from './Users.module.sass'
 
 const Users = ({ users, follow, unfollow, setUsers }) => {
   if (users.length === 0) {
-    setUsers([
-      {
-        id: 1,
-        fullName: 'Name1 Surname1',
-        status: 'website1.io',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        },
-        photoUrl: 'https://cutt.ly/Cr2Yc5d',
-        followed: true
-      },
-      {
-        id: 2,
-        fullName: 'Name2 Surname2',
-        status: 'website2.io',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        },
-        photoUrl: 'https://cutt.ly/Cr2Yc5d',
-        followed: false
-      },
-      {
-        id: 3,
-        fullName: 'Name3 Surname3',
-        status: 'website3.io',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        },
-        photoUrl: 'https://cutt.ly/Cr2Yc5d',
-        followed: false
-      },
-      {
-        id: 4,
-        fullName: 'Name4 Surname4',
-        status: 'website4.io',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        },
-        photoUrl: 'https://cutt.ly/Cr2Yc5d',
-        followed: true
-      },
-      {
-        id: 5,
-        fullName: 'Name5 Surname5',
-        status: 'website5.io',
-        location: {
-          city: 'Kiev',
-          country: 'Ukraine'
-        },
-        photoUrl: 'https://cutt.ly/Cr2Yc5d',
-        followed: true
-      }
-    ])
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        setUsers(response.data.items)
+      })
   }
 
   return (
@@ -70,31 +21,37 @@ const Users = ({ users, follow, unfollow, setUsers }) => {
           <div className={cls.user} key={user.id}>
             <span>
               <div className=''>
-                <img src={user.photoUrl} alt='user' />
+                <img
+                  src={user.photos.small ? user.photos.small : userPhoto}
+                  alt='user'
+                />
               </div>
               <div className=''>
-                {user.followed 
-                  ? <button
+                {user.followed ? 
+                  <button
                     className={'btnPrimary'}
                     onClick={() => unfollow(user.id)}
-                    > Unollow
-                    </button>
-                  : <button
-                      className={'btnPrimary'}
-                      onClick={() => follow(user.id)}
-                    > Follow
-                    </button>
+                  >
+                    Unollow
+                  </button>
+                 : 
+                  <button
+                    className={'btnPrimary'}
+                    onClick={() => follow(user.id)}
+                  >
+                    Follow
+                  </button>
                 }
               </div>
             </span>
             <span>
               <span>
-                <div className=''>{user.fullName}</div>
+                <div className=''>{user.name}</div>
                 <div className=''>{user.status}</div>
               </span>
               <span>
-                <div className=''>{user.location.city}</div>
-                <div className=''>{user.location.country}</div>
+                <div className=''>{'user.location.city'}</div>
+                <div className=''>{'user.location.country'}</div>
               </span>
             </span>
           </div>
