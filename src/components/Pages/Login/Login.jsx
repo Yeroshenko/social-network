41ca7780ac190a6af9b515ab6214e2f375783829
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-03-05 19:05:01 +0200

    fet: created Login page connected redux-form

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import { reduxForm, Field } from 'redux-form'
+
+import { Button } from '../../Ui'
+
+const LoginForm = ({ handleSubmit }) => {
+  return (
+    <form action='' onSubmit={handleSubmit}>
+      <Field type='text' name='login' placeholder='Login' component='input' />
+      <Field
+        component='input'
+        type='text'
+        name='password'
+        placeholder='Password'
+      />
+      <label>
+        <Field type='checkbox' name='rememberMe' component='input' />
+        remember me
+      </label>
+      <Button>Login</Button>
+    </form>
+  )
+}
+
+const LoginReduxForm = reduxForm({
+  form: 'login'
+})(LoginForm)
 
 const Login = () => {
-  return <div> Login </div>
+  const onSubmit = formData => {
+    console.log(formData)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  )
 }
 
 export default Login
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 
 import profileReducer from './profile-reducer'
@@ -13,15 +14,14 @@ const reducers = combineReducers({
   dialogsPage: dialogsReducer,
   asidePage: asideReducer,
   usersPage: usersReducer,
-  auth: authReducer 
+  auth: authReducer,
+  form: formReducer
 })
 
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ }) : compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
