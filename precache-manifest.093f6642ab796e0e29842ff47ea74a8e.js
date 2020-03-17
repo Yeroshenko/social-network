efa2ceabf3e16bdb78500d54a330072238ffa08a
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-03-17 15:36:05 +0200

    Updates

---
+++ precache-manifest.093f6642ab796e0e29842ff47ea74a8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6db931fd8a6bfc381ec26be32db138",
+    "url": "/socail-network/index.html"
+  },
+  {
+    "revision": "540fe36ee98a858e9b6a",
+    "url": "/socail-network/static/css/main.cef78df6.chunk.css"
+  },
+  {
+    "revision": "0abc05756a84fae5894b",
+    "url": "/socail-network/static/js/2.ad984531.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/socail-network/static/js/2.ad984531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540fe36ee98a858e9b6a",
+    "url": "/socail-network/static/js/main.7e435fcf.chunk.js"
+  },
+  {
+    "revision": "0a1eb448b0b50cc8064b",
+    "url": "/socail-network/static/js/runtime-main.2a442664.js"
+  },
+  {
+    "revision": "e1c0855b0babf7c6bf7a15749e2fe193",
+    "url": "/socail-network/static/media/avatar-substrate.e1c0855b.svg"
+  },
+  {
+    "revision": "230b83854503c44a425f229105982b9d",
+    "url": "/socail-network/static/media/logo.230b8385.svg"
+  },
+  {
+    "revision": "5bbc97f832477e3876d68a0e57185a19",
+    "url": "/socail-network/static/media/polygon.5bbc97f8.svg"
+  },
+  {
+    "revision": "d9362f6fe510a413998616579798c221",
+    "url": "/socail-network/static/media/user-wallpaper.d9362f6f.jpg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/socail-network/static/media/user.7cc7a630.png"
+  }
+]);
